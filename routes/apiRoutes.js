708acdbeaa558da389d6f5790f92bbe03b5e3114
Melissa.Kinsey <kinsey.melissa@gmail.com>
2021-03-11 16:50:48 -0600

    files: Move thre js files into Models folder. Install Mongoose.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,30 @@
-apiRoutes
+// Connect routes to data sources
+const createWorkout = require("../data/workouts");
+var getWorkoutsInRange = require("../data/api/workouts/range"); 
+
+// Handle API GET requests
+module.exports = function (app)
+{
+    app.get("/api/data/workouts", function (req, res)
+    {
+        res.json(createWorkout);
+    });
+    
+    app.get("/api/", function (req, res)
+    {
+        res.json(waitListData);
+    });
+    app.post("/api/tables", function (req, res)
+    {
+        if (tableData.length < 5)
+        {
+            tableData.push(req.body);
+            res.json(true);
+        }
+        else
+        {
+            waitListData.push(req.body);
+            res.json(false);
+        }
+    });
+}
